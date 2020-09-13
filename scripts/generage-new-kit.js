83c0d62167d78f1d5cdbf55950911ae990010523
s-ong-c <somony9292@gmail.com>
2020-09-13 17:21:06 +0900

    feat: scaffolding cli tool

---
+++ scripts/generage-new-kit.js
@@ -0,0 +1,24 @@
+
+const inquirer = require('inquirer');
+const sh = require('shelljs');
+
+(async function command() {
+  try {
+    const { type, name, description } = await inquirer.prompt([
+      { type: 'list', name: 'type', message: 'Project Type', choices: ['package', 'example'] },
+      { type: 'string', name: 'name', message: 'Project Name' },
+      { type: 'string', name: 'description', message: 'Project Description' },
+    ])
+
+    if (type === 'example') {
+      console.log('Not supported type');
+      return;
+    }
+    const packageName = `@cdd/${name}`
+
+    sh.exec(`npx hygen ${type} new --name ${packageName} --description ${description}`)
+  } catch (e) {
+    console.error(e)
+  }
+
+})();
