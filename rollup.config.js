5186932ca5add7f5ac2d81d61544de4c84370b45
s-ong-c <somony9292@gmail.com>
2020-09-13 17:16:58 +0900

    env: build process , ignore list update

---
+++ rollup.config.js
@@ -0,0 +1,55 @@
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+
+const isProduction = process.env.NODE_ENV = 'production'
+
+console.log(isProduction)
+
+function buildJS(input, output, format) {
+  const defaultOutputConfig = {
+    format,
+    exports: 'named',
+    sourcemap: true,
+  };
+  const esOutputConfig = {
+    ...defaultOutputConfig,
+    dir: output,
+  };
+  const cjsOutputConfig = {
+    ...defaultOutputConfig,
+    file: output,
+  };
+
+  return {
+    input,
+    output: [
+      format === 'es' ? esOutputConfig : cjsOutputConfig,
+    ],
+    plugins: [
+      resolve({
+        extensions,
+      }),
+      peerDepsExternal(),
+      commonjs(),
+      babel({
+        extensions,
+        runtimeHelpers: true,
+        rootMode: 'upward',
+      }),
+      json(),
+      url(),
+    ],
+    preserveModules: format === 'es',
+  };
+}
+
+export default [
+  buildJS('index.ts', 'dist/index.js', 'cjs'),
+  buildJS('index.ts','dist/esm', 'es'),
+];
