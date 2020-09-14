145f44251aade8a83fa0eaa8cca8f29fab11eac1
s-ong-c <somony9292@gmail.com>
2020-09-14 14:01:01 +0900

    chore:  fix eslint with yarn2  & storybook setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "commonjs": true,
-      "es2020": true
-  },
-  "extends": [
-      "eslint:recommended"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "ecmaVersion": 11
-  },
-  "plugins": [
-      "@typescript-eslint"
-  ],
-  "rules": {
-      "no-multiple-empty-lines": "error",
-      "comma-dangle": ["error", "always-multiline"],
-      "eol-last": ["error", "always"],
-      "semi": ["error", "never"],
-      "quotes": ["error", "single"],
-      "no-tabs": "error",
-      "padding-line-between-statements": [
-          "error",
-          { "blankLine": "always", "prev": "*", "next": "return" }
-      ]
+    'env': {
+      'browser': true,
+      'commonjs': true,
+      'es2020': true,
+    },
+    'extends': [
+      'eslint:recommended',
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+      'ecmaVersion': 11,
+    },
+    'plugins': [
+      '@typescript-eslint',
+    ],
+    'rules': {
+      'no-multiple-empty-lines': 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      'eol-last': ['error', 'always'],
+      'semi': 2,
+      'quotes': ['error', 'single'],
+      'no-tabs': 'error',
+      'padding-line-between-statements': [
+          'error',
+          { 'blankLine': 'always', 'prev': '*', 'next': 'return' },
+      ],
+    },
   }
-};
