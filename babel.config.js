09bd16fb525322691232808297b7ac195a4dc581
s-ong-c <somony9292@gmail.com>
2020-09-14 14:20:15 +0900

    fix: babel config file

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  presets: ["@babel/react", "@babel/typescript"],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-  ],
+  presets: ["@babel/env", "@babel/react", "@babel/typescript"],
+  plugins: [],
 };
